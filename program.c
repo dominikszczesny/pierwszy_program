4e4a493c28aa3066abe0721c484678376cb4c244
dominikszczesny <dominikszczesny5@wp.pl>
2017-10-19 16:04:57 +0200

    pierwszy program zakonczony

---
+++ program.c
@@ -0,0 +1,23 @@
+/*dolacz ninlioteke standardowa input-output*/
+#include <stdio.h>
+
+
+/* y = f(x) */
+
+/*  0 = main()  */
+
+int main()  /*    funkcja glowna programu, zwraca wartosc calkowita*/ 
+{ /* miedzy klamrami jest cialo funkcji */
+
+
+		puts("Pierwszy program");  /* funkcja z biblioteki stdio */
+
+                return 0; /*main zwraca wartosc 0*/
+
+
+}
+
+
+
+
+
